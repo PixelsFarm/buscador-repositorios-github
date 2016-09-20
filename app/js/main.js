561a5f9a29f6f8b17a30085768fa33f41d7e0135
Hector Pascual <info@pixelsfarm.com>
2016-09-20 23:10:20 +0200

    plantilla con loader añadida

--- app/js/main.js
+++ app/js/main.js
@@ -2,19 +2,27 @@
 // paginador-buscador
 //===================
 
+//modulo
 var miApp = angular.module('searchApp', ['angularUtils.directives.dirPagination']);
 
-miApp.controller('mainCtrl', function($scope, $http){
+//mainCtrl
+miApp.controller('mainCtrl', function($scope, $http) {
+	//vars
+	$scope.preload = false;
+
 	//toma datos json github. Solo deja mostrar 1000 resultados
 	$scope.cogerDatos = function() {
 		$scope.userNotFound = false;
 		$scope.loaded = false;
+		$scope.preload = true;
 
 		$http.get("https://api.github.com/search/repositories?q=" + $scope.repositories + "&page=1&per_page=100")
 			.success(function(data) {
-				$scope.total = data.total_count;
-				$scope.repositorio = data.items;
+				$scope.arrRepo = data; //guardamos datos json en array local
+				$scope.total = $scope.arrRepo.total_count; //total de reistros
+				$scope.repositorio = $scope.arrRepo.items; //accedemos al objeto "items" del array del repositorio
 				$scope.loaded = true;
+				$scope.preload = false;
 				console.log('repositorios encontrados');
 			})
 			.error(function(data) {
@@ -22,9 +30,7 @@ miApp.controller('mainCtrl', function($scope, $http){
 				$scope.errorMessage = 'Error de conexion con datos';
 				console.log('error de conexion');
 			});
-
 	}
-
 });
 
 
